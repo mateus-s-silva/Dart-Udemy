ef1a45e7f4264d88ab28f0af72337d7b7a72d87b
mateus-s-silva <mattahyah@gmail.com>
2023-01-18 00:07:26 -0300

    superando a de

---
+++ Fundamentos/operadores_04.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+
+main(){
+
+  print("Está chovendo: (s/N)");
+  bool estaChovendo = stdin.readLineSync() == "s";
+
+  print("Está frio: (s/N");
+  bool estaFrio = stdin.readLineSync() == "s";
+
+  String resultado =
+  estaChovendo || estaFrio ? "Ficar em casa" : "Sair!!!";
+
+  print(resultado);
+  
+  print(estaChovendo || estaFrio ? "Azarado" : "Sortudo");
+  
+
+
+
+}
