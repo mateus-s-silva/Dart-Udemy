ec5a6e7e88be2003de77b99d9bd0a82527c29bed
mateus-s-silva <mattahyah@gmail.com>
2023-01-19 00:43:42 -0300

    de poquin

---
+++ Fundamentos/constantes_02.dart
@@ -0,0 +1,13 @@
+main(){
+
+  var lista = const ['Mateus', 'Ana', 'Lia', 'Gui'];
+
+    lista = ['Banana', 'Maça']; 
+
+    lista.add('Rebeca');
+
+   
+
+  print(lista);
+
+}
---
+++ Fundamentos/generics.dart
@@ -0,0 +1,17 @@
+main(){
+
+  List<String> frutas = ['banana', 'maça', 'laranja'];
+  frutas.add('kiwi');
+
+  print(frutas);
+
+  Map<String, double> salarios = {
+    'Gerente': 5000.00,
+    'Vendedor': 3000.00,
+    'Estagiário': 700.50
+  };
+
+  print(salarios.isNotEmpty);
+
+
+}
