794102f3d165aadbe7258d0ed0581604dedf36f5
mateus-s-silva <mattahyah@gmail.com>
2023-02-01 05:11:58 -0300

    teste

--- controle/while.dart
+++ controle/while.dart
@@ -1,4 +1,25 @@
-main(){
+import 'dart:io';
 
+main(){
+  var digitado = '';
+  
+  int a = 0;
+
+  while(a < 10){
+    print(a);
+    a++;
+  }
+
+  while(digitado != 'sair'){
+    stdout.write('Escreva "sair" para sair: ');
+    digitado = stdin.readLineSync().toString(); 
+  }
+
+  do {
+    stdout.write('Escreva qualquer coisa, menos "sair", para sair do loop: ');
+    digitado = stdin.readLineSync().toString();
+  } while (digitado == 'sair');
+
+  print("Fim!");
   
 }
